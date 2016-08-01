59dc9b9485347c31f6795e893138103ad1e70ff8
SierraOCP <sramirez@open-contracting.org>
2016-07-31 22:47:17 -0400

    new improved R homework (added comments)

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These two functions create special spaces to hold and update caches related to inversed matrices, so they don't have to be recalculated.
 
-## Write a short comment describing this function
+##makeCacheMatrix creates a special "matrix", which is really a list containing a function to:
+
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the inverse
+#get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
@@ -17,9 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+##cacheSolve calculates the inverse of the special "matrix" created with the above function. 
+##However, it first checks to see if the inverse has already been calculated. 
+##If so, it gets the inverse from the cache and skips the computation. 
+##Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinverse function.
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
   if(!is.null(i)) {
@@ -30,5 +35,5 @@ cacheSolve <- function(x, ...) {
   i <- solve(data, ...)
   x$setinverse(i)
   i
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix 'i' that is the inverse of 'x'
 }
